913f749eb066fc162b7b25a0386f9d9bdc1a8e8e
MoisesPabloc <epabloc@miumg.edu.gt>
2025-06-03 21:59:44 -0600

    Update clock_divider.v

--- src/clock_divider.v
+++ src/clock_divider.v
@@ -1,14 +1,15 @@
 module clock_divider #(parameter DIVIDE_BY = 2) (
-    input clk,
-    output reg slow_clk
+    input  wire clk,
+    output reg  slow_clk = 0
 );
     reg [31:0] counter = 0;
 
     always @(posedge clk) begin
-        counter <= counter + 1;
         if (counter == DIVIDE_BY - 1) begin
-            counter <= 0;
+            counter  <= 0;
             slow_clk <= ~slow_clk;
+        end else begin
+            counter <= counter + 1;
         end
     end
 endmodule
